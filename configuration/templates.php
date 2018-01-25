9c72a83fe3b044807517babe7f5ecdd748f4f428
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-01-25 15:15:55 +1100

    Updated packager

--- configuration/templates.php
+++ configuration/templates.php
@@ -1,7 +1,7 @@
 <?php
 
 $templates['template1'] = array(
-    'folder_pattern' => 'custom/Extension/modules/{MODULENAME}',
+    'directory_pattern' => 'custom/Extension/modules/{MODULENAME}',
     'modules' => array(
         'Contacts' => 'Contact',
         'Accounts' => 'Account',
