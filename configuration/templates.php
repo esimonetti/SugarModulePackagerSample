a47cbce82b0b92c9fb8e94c36b3df4de877c3d23
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-05-09 11:39:27 +1000

    added Ext on the template, and moved one directory level

--- configuration/templates.php
+++ configuration/templates.php
@@ -1,7 +1,7 @@
 <?php
 
 $templates['template1'] = array(
-    'directory_pattern' => 'custom/Extension/modules/{MODULENAME}',
+    'directory_pattern' => 'custom/Extension/modules/{MODULENAME}/Ext',
     'modules' => array(
         'Contacts' => 'Contact',
         'Accounts' => 'Account',
